019b8721245e3a5a9a8240b6b8cbc207b4c24088
Jennifer <69029635+JENNIFER311@users.noreply.github.com>
2025-07-24 11:33:58 +0530

    Create variable_task.py

---
+++ python/variable_task.py
@@ -0,0 +1,12 @@
+#Task !:
+#Create a variable
+#Convert variable to int 
+#Convert int to float
+#Convert float to string 
+#Atlast print it as string
+
+number = 24
+convert_1 = int(number)
+convert_2 = float(convert_1)
+convert_3 = str(convert_2)
+print(convert_3)
